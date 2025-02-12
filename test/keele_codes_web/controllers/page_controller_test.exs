46a5bbb93f8b17be649d6e987acf978691ff8b98
mark <mark@keele.codes>
2025-02-12 11:54:57 -0700

    remove unused code

--- test/keele_codes_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule KeeleCodesWeb.PageControllerTest do
-  use KeeleCodesWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
-end
