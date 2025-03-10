1b5644fe75479fb02112d7b2143c62404f671fe3
mark <mark@keele.codes>
2025-03-10 12:19:14 -0600

    update release script

--- rel/scripts/release.sh
+++ rel/scripts/release.sh
@@ -1,7 +1,8 @@
 mix deps.get --only prod
 mix compile
+mix phx.digest.clean --all
 mix assets.deploy
 mix release --overwrite
 _build/prod/rel/keele_codes/bin/keele_codes eval "KeeleCodes.Release.migrate"
-_build/prod/rel/keele_codes/bin/keele_codes stop
-_build/prod/rel/keele_codes/bin/keele_codes start
+systemctl stop keele-codes
+systemctl start keele-codes
