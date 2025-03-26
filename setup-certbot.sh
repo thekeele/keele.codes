d1b4e4bbde3dd397cc815e5a498fc5cd96ab53bc
mark <mark@keele.codes>
2025-03-25 20:53:46 -0600

    update certbot script

--- setup-certbot.sh
+++ setup-certbot.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
+# Switch to SSL config
+cp nginx-ssl.conf nginx.conf
+
 # Start app and nginx on port 80
 docker compose up -d app nginx
 
 # Get certs
 docker compose up -d certbot
+
 docker exec keelecodes-certbot-1 certbot certonly \
   --webroot -w /var/www/html \
   -d keele.codes \
-  -m your-email@example.com \
+  -d www.keele.codes \
+  -m mark@keele.codes \
   --agree-tos --non-interactive
-
-# Stop nginx to swap configs
-docker compose stop nginx
-
-# Switch to SSL config
-cp nginx-ssl.conf nginx.conf
-
-# Restart with SSL
-docker compose up -d nginx
